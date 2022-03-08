4f4d492045b4fab0333f0c6e78a0b212088acb59
JavaPC <hariprasad.jha@kpis.in>
2022-03-08 15:32:34 +0530

    portfolio changes

---
+++ src/services/coverageService.js
@@ -0,0 +1,75 @@
+// All user related database operations can be defined here.
+
+import { SYSTEM_ERROR } from "../config/CONSTANTS";
+import axios from 'axios'
+import { COVERAGE_REST } from "./CONSTANTS";
+
+/**
+ * Function to fetch Make keyvalue.
+ */
+export const getMakeKeyValue = () => {
+  console.log("coverageService > getMakeKeyValue called...");
+  return new Promise((resolve, reject) => {
+    try {
+      axios
+        .get(COVERAGE_REST() + "/make")
+        .then((res) => {
+          console.log("getMakeKeyValue > axios res=", res);
+          resolve(res.data);
+        })
+        .catch((err) => {
+          console.log("getMakeKeyValue > axios err=", err);
+          reject("Error in getMakeKeyValue axios!");
+        });
+    } catch (error) {
+      console.error("in coverageService > getMakeKeyValue, Err===", error);
+      reject(SYSTEM_ERROR);
+    }
+  });
+};
+/**
+ * Function to fetch Model keyvalue.
+ */
+export const getModelKeyValue = () => {
+  console.log("coverageService > getModelKeyValue called...");
+  return new Promise((resolve, reject) => {
+    try {
+      axios
+        .get(COVERAGE_REST() + "/model")
+        .then((res) => {
+          console.log("getModelKeyValue > axios res=", res);
+          resolve(res.data);
+        })
+        .catch((err) => {
+          console.log("getModelKeyValue > axios err=", err);
+          reject("Error in getModelKeyValue axios!");
+        });
+    } catch (error) {
+      console.error("in coverageService > getModelKeyValue, Err===", error);
+      reject(SYSTEM_ERROR);
+    }
+  });
+};
+/**
+ * Function to fetch Prefix keyvalue.
+ */
+export const getPrefixKeyValue = () => {
+  console.log("coverageService > getPrefixKeyValue called...");
+  return new Promise((resolve, reject) => {
+    try {
+      axios
+        .get(COVERAGE_REST() + "/prefix")
+        .then((res) => {
+          console.log("getPrefixKeyValue > axios res=", res);
+          resolve(res.data);
+        })
+        .catch((err) => {
+          console.log("getPrefixKeyValue > axios err=", err);
+          reject("Error in getPrefixKeyValue axios!");
+        });
+    } catch (error) {
+      console.error("in coverageService > getPrefixKeyValue, Err===", error);
+      reject(SYSTEM_ERROR);
+    }
+  });
+};
