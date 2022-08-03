562fda22008b24969150cacca5221248ab74ad51
AyushiSharma <ayushi.sharma@kpis.in>
2022-08-03 18:29:59 +0530

    add components Repairoption01 and Segment01Disassemble

--- src/navigation/RouterConfig.js
+++ src/navigation/RouterConfig.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "pages/Dashboard";
 import { NotFound } from "navigation/NotFound";
-import { ROOT, RESET, VERIFY_EMAIL, WORK_LIST_NEW, SOLUTION_BUILDER_NEW, PERMISSION, PERMISSION_SETTING, SERVICE_NEW, LOGIN, PORTFOLIO_SUMMARY, ANALYTICS, WORK_LIST, REPORTS, PROFILE, DASHBOARD, PAGE1, AUTH_PAGE1, TEST_REACT_TABLE_NESTED, GUIDED_SOLUTION_BUILDER, PORTFOLIO_AND_BUILDER_NEW, SOLUTION_BUILDER_ANALYTICS, SOLUTION_BUILDER_SERVICE_PORTFOLIO, ACCOUNT, BULID_REPAIR_OPTION, RIPAIR_SEGMENT01_TRANSMISSION, RIPAIR_SERVICE_ESTIMATE, WITHOUTSPARE_REPAIR_OPTION, PART_LIST, STANDARD_JOBS, KITS, } from "navigation/CONSTANTS";
+import { ROOT, RESET, VERIFY_EMAIL, WORK_LIST_NEW, SOLUTION_BUILDER_NEW, PERMISSION, PERMISSION_SETTING, SERVICE_NEW, LOGIN, PORTFOLIO_SUMMARY, ANALYTICS, WORK_LIST, REPORTS, PROFILE, DASHBOARD, PAGE1, AUTH_PAGE1, TEST_REACT_TABLE_NESTED, GUIDED_SOLUTION_BUILDER, PORTFOLIO_AND_BUILDER_NEW, SOLUTION_BUILDER_ANALYTICS, SOLUTION_BUILDER_SERVICE_PORTFOLIO, ACCOUNT, BULID_REPAIR_OPTION, RIPAIR_SEGMENT01_TRANSMISSION, RIPAIR_SEGMENT01_DISASSEMBLE, RIPAIR_OPTION01, RIPAIR_SERVICE_ESTIMATE, WITHOUTSPARE_REPAIR_OPTION, PART_LIST, STANDARD_JOBS, KITS, } from "navigation/CONSTANTS";
 import { Analytics, ServicePortfolio, SolutionBuilderCreate } from "../pages/SolutionModules/index"
 import { Profile } from '../pages/User/index'
 import { CreatePortfolio, WorkList, CreateWorkList, PortfolioSummary } from "../pages/PortfolioAndBundle/index"
@@ -18,6 +18,8 @@ import { VerifyEmail } from "../pages/LoginSignUp/index"
 import { Account } from "pages/User/Account";
 import BulidRepairOptions from "pages/Repair/BulidRepairOptions";
 import Segment01Transmission from "pages/Repair/Segment01Transmission";
+import Segment01Disassemble from "pages/Repair/Segment01Disassemble";
+import RepairOption01 from "pages/Repair/RepairOption01";
 import RepairServiceEstimate from "pages/Repair/RepairServiceEstimate";
 import WithoutSpareParts from "pages/Repair/WithoutSpareParts";
 import PartList from "pages/Repair/PartList";
@@ -51,6 +53,8 @@ export const RouterConfig = () => {
         <Route exact path={ACCOUNT} component={Account} />
         <Route exact path={BULID_REPAIR_OPTION} component={BulidRepairOptions} />
         <Route exact path={RIPAIR_SEGMENT01_TRANSMISSION} component={Segment01Transmission} />
+        <Route exact path={RIPAIR_SEGMENT01_DISASSEMBLE} component={Segment01Disassemble} />
+        <Route exact path={RIPAIR_OPTION01} component={RepairOption01} />
         <Route exact path={RIPAIR_SERVICE_ESTIMATE} component={RepairServiceEstimate} />
         <Route exact path={WITHOUTSPARE_REPAIR_OPTION} component={WithoutSpareParts} />
         <Route exact path={PART_LIST} component={PartList} />
