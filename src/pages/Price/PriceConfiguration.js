556863890c763d8a35e25ce3aef2ea0f319cb010
AyushiSharma <ayushi.sharma@kpis.in>
2022-11-28 18:55:56 +0530

    ui changes

--- src/pages/Price/PriceConfiguration.js
+++ src/pages/Price/PriceConfiguration.js
@@ -82,7 +82,7 @@ function PriceConfiguration(){
   ];
     return(
       <>
-       <CommanComponents/>
+       {/* <CommanComponents/> */}
       <div className="content-body" style={{ minHeight: '884px' }}>
         <div class="container-fluid mt-3">
            <div className="d-flex align-items-center justify-content-between mt-2">
