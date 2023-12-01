45a17b10e4a7ccf0a46764eca5096d83b3c5a006
pinaki-gm <pinaki.panigrahy@grandes-mentes.com>
2023-12-01 17:15:32 +0530

    Filter Changes

---
+++ src/pages/Insights/SelectBox.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function SelectBox(props) {
+    let { options } = props;
+    return (
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel >{props.label}</InputLabel>
+            <Select
+                value={props.value}
+                label={props.label}
+                onChange={props.handleChange}
+                size='small'
+            >
+                {options?.map(option =>
+                    <MenuItem value={option}>{option}</MenuItem>
+                )}
+            </Select>
+        </FormControl>
+    );
+}
